71320934c3acf9445feabdd17110f1eb6f66ada6
jlp0328 <jlp0328@gmail.com>
2017-03-17 13:09:11 -0400

    burger model created

--- models/burger.js
+++ models/burger.js
@@ -1,26 +1,23 @@
-var orm = require("../config/orm.js");
-
-var burger = {
-
-  selectAll: function(cb){
-    orm.selectAll("burgers", function(res){
-      cb(res);
+module.exports = function(sequelize, DataTypes){
+    var Burger = sequelize.define("Burger",
+    {
+      burger_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        //AllowNull is a flag that restricts a todo from being entered if it doesn't
+        validate:{
+          //len is a validation that checks that our todo is btwn 1 and 140 characters
+          len:[1, 140]
+        }
+      },
+      devoured: {
+        type: DataTypes.BOOLEAN,
+        //defaultValue is a flag that defaults a new todos complete value to false if it isn't supllied one
+        defaultValue: false
+      }
     });
-  },
+    return Todo;
+    //Anything that runs this function and returns
+    //Make variable consistent with the filename
 
-  insertOne: function(cols, vals, cb){
-    orm.insertOne("burgers", cols, vals, function(res){
-      cb(res);
-    });
-  },
-
-  updateOne: function(objColVals, condition, cb){
-    orm.updateOne("burgers", objColVals, condition, function(res){
-      cb(res);
-    });
-  }
-
-}
-
-
-module.exports = burger;
+};
